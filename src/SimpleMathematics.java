d9d537fdd250caa648750e21c6ac6d1d815b76bd
porkpiie <adtready@gmail.com>
2019-06-16 10:34:58 +0100

    removed variable lines into one, renamed Test to something useful

--- src/SimpleMathematics.java
+++ src/SimpleMathematics.java
@@ -1,17 +1,15 @@
 class Maths{
     public void addition(int a, int b){
-        int result;
-        result=a+b;
+        int result = a + b;
         System.out.println("Result: " + result);
     }
     public int subtraction(int a, int b){
-        int answer;
-        answer = a-b;
+        int answer = a - b;
         return answer;
     }
 }
 
-public class Test3 {
+public class SimpleMathematics {
     public static void main(String[] args) {
         Maths x;
         x = new Maths();
